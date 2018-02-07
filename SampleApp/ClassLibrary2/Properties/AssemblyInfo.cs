8f0a9b07a0fd9e998a464c02d7d63a94db380525
Pradeep Pradyumna <pradeepradyumna@gmail.com>
2018-02-07 17:32:13 +0530

    LRx86 - All Assemblies - incremented assembly version

--- SampleApp/ClassLibrary2/Properties/AssemblyInfo.cs
+++ SampleApp/ClassLibrary2/Properties/AssemblyInfo.cs
@@ -5,12 +5,12 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("SampleApp")]
+[assembly: AssemblyTitle("ClassLibrary2")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("SampleApp")]
-[assembly: AssemblyCopyright("Copyright ©  2017")]
+[assembly: AssemblyProduct("ClassLibrary2")]
+[assembly: AssemblyCopyright("Copyright ©  2018")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
@@ -20,7 +20,7 @@ using System.Runtime.InteropServices;
 [assembly: ComVisible(false)]
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
-[assembly: Guid("6c681acd-98bd-41c3-8d71-bae66ab09817")]
+[assembly: Guid("128cadd9-ff83-40a4-b15d-d805640497f9")]
 
 // Version information for an assembly consists of the following four values:
 //
